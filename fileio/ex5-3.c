a4c15b3c9cfc8e4df4cf1de377dea8cc743b9612
Ajit Ananthadevan <ntn.ajit@gmail.com>
2018-11-19 22:24:24 +1100

    done exercise 5-3 and 5-4

---
+++ fileio/ex5-3.c
@@ -0,0 +1,51 @@
+
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "tlpi_hdr.h"
+
+int main (int argc, char *argv[])
+{
+  int fd, flags, i, numWritten, len, ap;
+  _Bool apnd_flag = 0;
+
+  printf ("Hello world\n");
+
+    for (ap = 1; ap < argc; ap++) {
+    if (argv[ap][0] == 'x') {
+      apnd_flag = 1;
+      break;
+    }
+  }
+
+    if (apnd_flag)
+      flags=O_RDWR | O_CREAT; //seek to end later
+    else
+      flags = O_RDWR | O_CREAT | O_APPEND;
+
+
+    fd = open(argv[1], flags,
+                S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
+                S_IROTH | S_IWOTH);                     /* rw-rw-rw- */
+    if (fd == -1)
+      errExit("open");
+
+  if (apnd_flag)
+    lseek(fd, 0, SEEK_END);
+
+  len = getLong(argv[2], GN_ANY_BASE, argv[2]);
+  //printf ("%d", len);
+  for(i = 0; i < len; i++) {
+    numWritten = write (fd, "H", 1);
+    if (numWritten == -1)
+      errExit("write");
+  }
+    
+
+  if (close(fd) == -1)
+    errExit("close");
+
+  exit(EXIT_SUCCESS);
+
+
+}
---
+++ fileio/ex5-4.c
@@ -0,0 +1,43 @@
+/* exercise 5-4.c */
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include "tlpi_hdr.h"
+
+int my_dup (int old_fd)
+{
+  return fcntl(old_fd, F_DUPFD, 0);
+}
+
+int my_dup2 (int old_fd, int new_fd)
+{
+  if(!fcntl(old_fd, F_GETFL)) {
+    errno = EBADF;
+    return -1;
+  }
+
+  //else if(new_fd == old_fd)
+  //return old_fd;
+
+  close(new_fd);
+  return fcntl(old_fd, F_DUPFD, new_fd);
+}
+
+int main (void)
+{
+  int fd;
+
+  //close(1);
+  //i = my_dup(2);
+
+  fd = my_dup2(600, 600);
+  if(fd == -1)
+    errExit("my_dup2");
+
+  printf("%d\n", fd);
+
+  close(fd);
+
+  return 0;
+}
